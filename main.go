0d17f25cad5a2952a713abb57b5c95f2b0a6a516
Kevin <50965376+tgodkev@users.noreply.github.com>
2024-01-21 03:03:21 -0500

    some changes to main function and vars

--- main.go
+++ main.go
@@ -3,39 +3,44 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/containerd/containerd/v2/core/images/converter"
 	"github.com/disintegration/imaging"
 	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
+)
+
+var (
+	dir     = flag.String("dir", ".", "Directory to process images")
+	resize  = flag.String("resize", "0x0", "Target size for resizing, formatted as WIDTHxHEIGHT. Use '0x0' to skip resizing.")
+	quality = flag.Int("quality", 75, "Compression quality (1-100)")
+	format  = flag.String("format", "jpg", "Target format for conversion (jpg, png, gif, webp)")
 )
 
 func main() {
-	var (
-		format             string
-		compressionQuality int
-		targetFormat       string
-		cropSize           string
-	)
-
-	// Define and parse command-line flagear
-
-	flag.StringVar(&format, "format", "jpg", "Image format to process (default: jpg)")
-	flag.IntVar(&compressionQuality, "quality", 75, "Compression quality in percentage (default: 75)")
-	flag.StringVar(&targetFormat, "convert", "webp", "Format to convert to (default: webp)")
-	flag.StringVar(&cropSize, "crop", "", "Size to crop images to ex: '300x300' (height x width) (default: '') ")
-
 	flag.Parse()
 
-	// Image processing logic
-	dir, _ := os.Getwd()
-	filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if filepath.Ext(path) == "."+format {
-			ProcessImage(path, cropSize, compressionQuality, targetFormat)
+	// Validate inputs...
+
+	files, err := os.ReadDir(*dir)
+	if err != nil {
+		fmt.Printf("Failed to read directory: %v\n", err)
+		return
+	}
+
+	var wg sync.WaitGroup
+	for _, file := range files {
+		if file.IsDir() {
+			continue
 		}
-		return nil
-	})
+		wg.Add(1)
+		go func(fileName string) {
+			defer wg.Done()
+			ProcessImage(fileName, *resize, *quality, *format)
+			// Add your logic to handle resizing, compression, and conversion
+		}(file.Name())
+	}
+	wg.Wait()
 }
 
 // ProcessImage - function for image processing
